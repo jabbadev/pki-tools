1c37f2bd03c41db6398c32bb291d62ceeb4e433d
Francesco Dalprà <francesco.dalpra@intesasanpaolo.com>
2024-02-26 18:41:14 +0100

    test tls refacoring

--- __test__/tls.test.js
+++ __test__/tls.test.js
@@ -1,23 +1,50 @@
 import { describe, expect, test, it, beforeAll, afterAll } from '@jest/globals'
-import { generateCaRootCertificate, generatePkiCertificate } from '../lib/pkiutils.js'
+import { generateCaRootCertificate, generatePkiCertificate, generateRsaPemKeys } from '../lib/pkiutils.js'
 import { CertificateSubject } from '../lib/commons.js'
 import tls from 'node:tls'
+import { error } from 'node:console'
 
-const caRoot = await generateCaRootCertificate(
-    new CertificateSubject("/CN=Root CA/C=IT/ST=Italy/L=Bergamo/O=MyNET/OU=MyNET Root CA server/E=ca-root@mynet.it"),
-    "01",
-    { keySize: 2048 }
-)
+let smallRefusedKeys, minimumAcceptedKeys, caRoot, serverCert, clientCert, tlsServer, tlsPort 
+beforeAll(()=>{
 
-const serverCert = await generatePkiCertificate(
-    new CertificateSubject("/CN=server.mynet.it/C=IT/ST=Italy/L=Bergamo/O=MyNET/OU=MyNET Web Server/E=server@mynet.it"),
-    "02",caRoot.certificate.data,caRoot.privateKey,{ keySize: 512 }
-)
+    return Promise.all([
+        generateRsaPemKeys({ bits: 512 }).then( keys => {
+            smallRefusedKeys = keys
+        }),
 
-const clientCert = await generatePkiCertificate(
-    new CertificateSubject("/CN=client.mynet.it/C=IT/ST=Italy/L=Bergamo/O=MyNET/OU=MyNET TLS Client/E=client@mynet.it"),
-    "03",caRoot.certificate.data,caRoot.privateKey,{ keySize: 512 }
-)
+        generateRsaPemKeys({ bits: 1024 }).then( keys => {
+            minimumAcceptedKeys = keys
+        }),
+
+        generateCaRootCertificate(
+            new CertificateSubject("/CN=Root CA/C=IT/ST=Italy/L=Bergamo/O=MyNET/OU=MyNET Root CA server/E=ca-root@mynet.it"),
+            "01",
+            { keySize: 1024 }).then( data => {
+                caRoot = data
+            }) ])
+    .then(() => Promise.all([
+
+            generatePkiCertificate(
+                new CertificateSubject("/CN=server.mynet.it/C=IT/ST=Italy/L=Bergamo/O=MyNET/OU=MyNET Web Server/E=server@mynet.it"),
+                "02",caRoot.certificate.data,caRoot.privateKey,{ keySize: 512 }
+            ).then( data => {
+                serverCert = data
+            }),
+
+            generatePkiCertificate(
+                new CertificateSubject("/CN=client.mynet.it/C=IT/ST=Italy/L=Bergamo/O=MyNET/OU=MyNET TLS Client/E=client@mynet.it"),
+                "03",caRoot.certificate.data,caRoot.privateKey,{ keySize: 512 }
+            ).then( data => {
+                clientCert = data
+            })
+
+    ]))
+    .then(()=> TLSServer({ key: serverCert.privateKey,
+                          cert: serverCert.certificate.data,
+                          pemCaCert: caRoot.certificate.data,  })
+    )
+    .then((tlsServerInfo) => { tlsServer = tlsServerInfo.server, tlsPort = tlsServerInfo.port })
+})
 
 const getRandomPort = (min, max) => {
     const minCeiled = Math.ceil(min)
@@ -25,43 +52,86 @@ const getRandomPort = (min, max) => {
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
 }
 
-const MAX_RETRY = 10
-let tlsServer, tlsPort = getRandomPort(7000,9000), retry = MAX_RETRY, timer = null, tslStatus = "tls server unavailable"
-const initializeTls = () => new Promise(async (resolve,reject) => {
-    const options = {
-        key: serverCert.privateKey,
-        cert: serverCert.certificate.data ,
-        requestCert: true,
-        ca: [ caRoot.certificate.data ],
-    };
 
-    tlsServer = tls.createServer(options, (socket) => {
-        expect(socket.authorized).toBeTruthy()
-        socket.write('welcome!\n');
-        socket.setEncoding('utf8');
-        socket.pipe(socket);
-    })
- 
-    tlsServer.listen(tlsPort, () => {
-        tslStatus = "tls server ready"
-        resolve()
-    })
+const TLSServer = (tlsOptions) => new Promise((resolve,reject)=>{
+    try {
+        const defaultOptions = {
+            minPort: 7000,
+            maxPort: 9000,
+            retry: 10
+        }
+        const options = { ...defaultOptions, ...tlsOptions }
+        
+        let tlsPort = getRandomPort(7000,9000),
+            tlsServer
 
-    tlsServer.on('error',(error)=>{
-        if ( error.code === 'EADDRINUSE'){
-            retry -= 1
-            if ( !retry ){
-                //tlsServer.close()
-                tslStatus = `${tslStatus} - max retry [${MAX_RETRY}] to find free listening port`
-                reject("all port in use")
-            }
-            else {
-                tlsPort = getRandomPort(7000,9000)
-                tlsServer.listen(tlsPort)
+        const tlsconf = {
+            ca: [ options.pemCaCert ]
+        }
+
+        if ( options.pfx ) {
+            tlsconf.pfx = options.pfx
+            if ( options.passphrase ) {
+                tlsconf.passphrase = options.passphrase
+            } else {
+                throw new Error("pfx need passphrase")
             }
         }
-    })
+        else {
+            
+            if ( options.key ){
+                tlsconf.key = options.key
+            }
+            else {
+                throw new Error("private key undefined")
+            }
+
+            if ( options.cert ){
+                tlsconf.cert  = options.cert
+            }
+            else {
+                throw new Error("certificate undefined")
+            }
+            
+            if ( options.passphrase ) {
+                tlsconf.passphrase = options.passphrase
+            }
+
+            tlsconf.requestCert = true
+        }
+
+        tlsServer = tls.createServer(tlsconf, (socket) => {
+            expect(socket.authorized).toBeTruthy()
+            socket.write('welcome!\n');
+            socket.setEncoding('utf8');
+            socket.pipe(socket);
+        })
+     
+        tlsServer.listen(tlsPort, () => {
+            tslStatus = "tls server ready"
+            resolve({ server: tlsServer, port: tlsPort })
+        })
+    
+        tlsServer.on('error',(error)=>{
+            if ( error.code === 'EADDRINUSE'){
+                retry -= 1
+                if ( !retry ){
+                    //tlsServer.close()
+                    tslStatus = `${tslStatus} - max retry [${MAX_RETRY}] to find free listening port`
+                    reject("all port in use")
+                }
+                else {
+                    tlsPort = getRandomPort(7000,9000)
+                    tlsServer.listen(tlsPort)
+                }
+            }
+        })
+    }
+    catch(error) { reject(error) }
 })
+
+const MAX_RETRY = 10
+let /*tlsServer, tlsPort = getRandomPort(7000,9000),*/ retry = MAX_RETRY, timer = null, tslStatus = "tls server unavailable"
 
 const execTlsRequest = () => new Promise((resolve,reject)=>{
 
@@ -92,9 +162,10 @@ const execTlsRequest = () => new Promise((resolve,reject)=>{
 
 describe("test a tls server with PKI certificate",()=>{
 
+    /*
     beforeAll(() => {
         return initializeTls()
-    })
+    })*/
 
     it("exec client request",async ()=>{
         await expect(execTlsRequest()).resolves.toEqual('welcome!\n')
